da45aa05f8f23b6045cb3cd4d7943226ccf95ca3
Hanan <h.ur-rehman@wafios.de>
2024-02-05 16:43:43 +0100

    WIP: Add/delete from a cart

--- test/spec/tests.js
+++ test/spec/tests.js
@@ -1,4 +1,4 @@
-describe("First test", () => {
+describe("My Demo App tests", () => {
   it("This is first test", async () => {
     await $(
       '//android.view.ViewGroup[@content-desc="open menu"]/android.widget.ImageView'
@@ -8,5 +8,16 @@ describe("First test", () => {
       '//android.widget.TextView[@text="Catalog"]'
     );
     await expect(menuItemCatalog).toHaveText("Catalog");
+  });
+
+  it.only("Add/delete product from cart", async () => {
+    await $(
+      '//android.widget.TextView[@content-desc="store item text" and @text="Sauce Labs Backpack"]'
+    ).click();
+    await $('//android.widget.TextView[@text="Add To Cart"]').click();
+    await $(
+      '//android.view.ViewGroup[@content-desc="cart badge"]/android.widget.ImageView'
+    ).click();
+    expect(await $("~product label")).toHaveText("Sauce Labs Backpack");
   });
 });
