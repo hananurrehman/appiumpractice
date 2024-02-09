9c6508ec793fe3a3bf190e03127118d15bb6b738
Hanan <h.ur-rehman@wafios.de>
2024-02-09 15:40:19 +0100

    Fixed first test bug

--- test/spec/tests.js
+++ test/spec/tests.js
@@ -1,18 +1,17 @@
 const pomPage = require("../page-object/pom.page");
 describe("My Demo App tests", () => {
   it("This is first test", async () => {
-    (await pomPage.leftMenuBtn).click;
+    await pomPage.leftMenuBtn.click();
     await expect(pomPage.menuItemCatalog).toHaveText("Catalog");
-    //This may seem odd but for now webdriver is unable to find this element.
-    //This is evident from the fact that in the Appium inspector I was not able to select the left menu items using the "Select Elements tool"
   });
+});
 
-  it("Add/delete product from cart", async () => {
-    await pomPage.firstItem.click();
-    await pomPage.addToCartBtn.click();
-    await pomPage.cartBtn.click();
-    await expect(await pomPage.productLabel).toHaveText("Sauce Labs Backpack");
-    await pomPage.removeItemBtn.click();
-    await expect(await pomPage.noItemsText).toExist();
-  });
+it("Add/delete product from cart", async () => {
+  //await driver.executeScript("mobile: pressKey", [{ keycode: 4 }]);
+  await pomPage.firstItem.click();
+  await pomPage.addToCartBtn.click();
+  await pomPage.cartBtn.click();
+  await expect(await pomPage.productLabel).toHaveText("Sauce Labs Backpack");
+  await pomPage.removeItemBtn.click();
+  await expect(await pomPage.noItemsText).toExist();
 });
